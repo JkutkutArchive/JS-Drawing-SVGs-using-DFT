c12287ca6d2f73a3945db65004f97fcc0caea7bd
jkutkut <jorgeregonzalez@gmail.com>
2020-06-30 00:30:02 +0200

    Logic to add svg drawings in progress

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@
 // Code based on The Coding Train code:
 // https://thecodingtrain.com/CodingChallenges/130-fourier-transform-drawing.html
 
+//      FUNCTIONS:
 
 function dft(f) { // Calculate the DFT of the given function f (f is a array of )
     const F = []; //The DFF of the given function f
@@ -26,29 +27,6 @@ function dft(f) { // Calculate the DFT of the given function f (f is a array of 
     return F;
 }
 
-let x = [];
-let y = [];
-let fourierX;
-let fourierY;
-let time = 0;
-let path = [];
-let vx, vy, v;
-let drew = false;
-
-function setup() {
-    createCanvas(900, 800);
-    const skip = 8;
-    for (let i = 0; i < drawing.length; i += skip) {
-        x.push(drawing[i].x);
-        y.push(drawing[i].y);
-    }
-    fourierX = dft(x); // Do the DFT of the function x(x)
-    fourierY = dft(y); // Do the DFT of the function y(x)
-
-    fourierX.sort((a, b) => b.amp - a.amp); //sort the waves by their amp
-    fourierY.sort((a, b) => b.amp - a.amp);
-}
-
 function epiCycles(x, y, rotation, fourier) {
     for (let i = 0; i < fourier.length; i++) {
         let prevx = x, prevy = y; // Center of the current circle 
@@ -68,6 +46,66 @@ function epiCycles(x, y, rotation, fourier) {
         line(prevx, prevy, x, y); // Draw radius
     }
     return createVector(x, y); // Return the end of the last radius
+}
+
+function updateFourier(){
+    const skip = 8;
+    for (let i = 0; i < drawing.length; i += skip) {
+        x.push(drawing[i].x);
+        y.push(drawing[i].y);
+    }
+    fourierX = dft(x); // Do the DFT of the function x(x)
+    fourierY = dft(y); // Do the DFT of the function y(x)
+
+    fourierX.sort((a, b) => b.amp - a.amp); //sort the waves by their amp
+    fourierY.sort((a, b) => b.amp - a.amp);
+}
+
+function appendFile(file){
+    print(file.name);
+    try {
+        if (file.type != "image"){
+            throw "The file must be a svg image";
+        }
+        if (!RegExp("\.+\\.svg$").test(file.name)){
+            throw "The file must be a .svg image, not just an image";
+        }
+    } catch (error) {
+        print("Error");
+        print(error);
+    }
+    // if here, the file should be correct
+}
+
+//      VARIABLES:
+
+let x = []; // x coordinates of the ideal figure
+let y = []; // y coordinates of the ideal figure
+let fourierX; // Xn.x coordinates of the fourier figure
+let fourierY; // Xn.y coordinates of the fourier figure
+let time = 0;
+let path = []; // Array of coordinates of points to represent
+let vx, vy, v; // (in order) endCoord of the last radius in each function and a vector of the corresponding point 
+let drew = false; // When all figure represented, this becomes true => the path is showed but no more points are added
+
+// UI:
+var fSelector, fileInput;
+
+//      CODE:
+
+function setup() {
+    createCanvas(900, 800);
+
+    fSelector = createSelect();
+    fSelector.position(20, 20);
+    fSelector.option("Train");
+
+    fSelector.changed(updateFourier); //When changed the element selected, update
+
+    fileInput = createFileInput(appendFile, false); //When file selected, execute appendFile (only 1 file per selection)
+
+    // Fourier code: 
+    updateFourier();
 }
 
 function draw() {
