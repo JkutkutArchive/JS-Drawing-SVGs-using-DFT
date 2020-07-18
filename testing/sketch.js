1153025f4f33fb2cb41ea3b0c886c6f9b45d2444
jkutkut <jorgeregonzalez@gmail.com>
2020-07-19 00:28:15 +0200

    it worksgit push

--- testing/sketch.js
+++ testing/sketch.js
@@ -7,22 +7,36 @@ function setup(){
     fileInput = createFileInput(appendFile, false); //When file selected, execute appendFile (only 1 file per selection)
 }
 function draw(){
-    background(100);
+    background(200);
+    if (points.length != 0){
+        stroke(0); // Change the color
+        strokeWeight(3); // Make the points 10 pixels in size
+        for (let i = 0; i < points.length; i++){
+            point(points[i].x, points[i].y);
+        }
+    }
 }
 
 function svgToPoints(fileText){
     // let w, h;
-    let w = 800, h = 800;
-    try{
-        // let parser = new DOMParser();
-        console.log(fileText)
-        // let doc = parser.parseFromString(current_svg_xml, "application/xml");
-        // var paths = doc.getElementsByTagName("path");
-        // current_displayed_paths = paths;
-
+    let w = 1000, h = 1000;
+    // try{
+    let text = "";
+    for(let t = 0; t < fileText.length; t++){
+        text += fileText[t] + "\n";
     }
-    catch{
+    console.log(text)
 
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(text, "text/xml");
+    console.log(doc)
+    let path = doc.getElementsByTagName("path")[0];
+    console.log(path)
+    
+    for ( var i=0,l=300; i<l; i++ ){
+        var p = path.getPointAtLength( i/l * path.getTotalLength() );
+        console.log(p);
+        points.push(p);
     }
     createCanvas(w, h);
 }
